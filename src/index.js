0d11909ee0e3afd0c74da3daaf3918a3a4688902
alexeyinn <alexey.i.nn@gmail.com>
2020-12-14 19:30:51 +0300

    Пользователь задает параметры модели

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+//Пользователь вводит значения отображаемого куба
+let boxLength = prompt(
+  "Веедите длину прямоугольника. (предпочтительно значение от 1 до 10)",
+  5
+);
+
+let boxWidth = prompt(
+  "Веедите ширину прямоугольника. (предпочтительно значение от 1 до 10)",
+  5
+);
+let boxHeight = prompt(
+  "Веедите высоту прямоугольника. (предпочтительно значение от 1 до 10)",
+  5
+);
+
 const scene = new THREE.Scene();
+// Задается вид "камеры", смотрящий на созданный объект. Задаются ее параметры.
 const camera = new THREE.PerspectiveCamera(
   30,
   window.innerWidth / window.innerHeight,
@@ -9,16 +25,22 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-//обозначаем размеры куба
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+// Обозначаем размеры прямоугольника
+const geometry = new THREE.BoxBufferGeometry(
+  `${boxLength}`,
+  `${boxHeight}`,
+  `${boxWidth}`
+);
+// Задается цвет созданной модели, так же можно вставить изображения.
 const material = new THREE.MeshBasicMaterial({ color: 0x808080 });
 const cube = new THREE.Mesh(geometry, material);
-//высталвение углов поворота куба
-cube.rotation.set(0.2, 0.7, 0); //должно обязательно стоять перед добавлением куба в сцену
+// Высталвение углов поворота отображаемой модели
+cube.rotation.set(0.2, 0.7, 0); // Должно обязательно стоять перед добавлением модели в сцену
 
 scene.add(cube);
+// Перемещение позиции "камеры", в данном случае по оси Z.
+// Так же можно задать угол ее наклона через параметры .rotation
+camera.position.z = 30;
 
-camera.position.z = 10;
-
-//рендер объекта
+// Рендер объекта
 renderer.render(scene, camera);
